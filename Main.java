4c062465fe4350b0a664ea8e9b3c625003370f2a
AxelGarciaTello <gata21022000@hotmail.com>
2019-10-21 19:36:54 -0500

    Metodo main

---
+++ Main.java
@@ -0,0 +1,25 @@
+/*
+Instituto Politécnico Nacional
+Escuela Superior de Cómputo
+Teoria Computacional
+Grupo: 2CV1
+Alumno: García Tello Axel
+Profesor: Benjamin Luna Benoso
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Lector de AFP
+
+Fecha: 21 de octubre de 2019
+*/
+
+
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        AFP inicio=new AFP();
+        inicio.iniciarAFP();
+    }
+    
+}
